71b71463d782aec8c238212d8a2224864a0584c2
Erdem Tsynduev <erdemtsynduev@mail.ru>
2018-07-05 20:13:42 +0300

    Add Api get recipes

--- app/src/main/java/com/erdemtsynduev/homeworkbakingapp/network/api/Api.java
+++ app/src/main/java/com/erdemtsynduev/homeworkbakingapp/network/api/Api.java
@@ -1,6 +1,10 @@
 package com.erdemtsynduev.homeworkbakingapp.network.api;
 
+import retrofit2.http.GET;
+
 public interface Api {
 
+    @GET("topher/2017/May/59121517_baking/baking.json")
+    Call<List<Recipe>> getRecipes();
 
 }
