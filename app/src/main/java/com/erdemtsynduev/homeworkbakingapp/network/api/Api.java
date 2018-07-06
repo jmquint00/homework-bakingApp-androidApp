b0544b13a314c4c7cfe2acfbda7b6738d5091a01
Erdem Tsynduev <erdemtsynduev@mail.ru>
2018-07-06 12:39:09 +0300

    Add models for api

--- app/src/main/java/com/erdemtsynduev/homeworkbakingapp/network/api/Api.java
+++ app/src/main/java/com/erdemtsynduev/homeworkbakingapp/network/api/Api.java
@@ -1,10 +1,14 @@
 package com.erdemtsynduev.homeworkbakingapp.network.api;
 
+import com.erdemtsynduev.homeworkbakingapp.network.models.Recipe;
+
+import java.util.List;
+
+import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface Api {
 
     @GET("topher/2017/May/59121517_baking/baking.json")
     Call<List<Recipe>> getRecipes();
-
 }
